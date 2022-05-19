e16ecc490d158e416652c5a5f1113951827ee17d
lydiacho <sheecho0804@ewhain.net>
2022-05-20 00:33:37 +0900

    fandom table 생성

---
+++ sqlfile.sql
@@ -0,0 +1,17 @@
+SELECT * FROM gimmeduck_db.fandom_table;
+insert into fandom_table (idx, fandom) values (1, '아미');
+insert into fandom_table (fandom) values ('다이브');
+insert into fandom_table (fandom) values ('레베럽');
+insert into fandom_table (fandom) values ('캐럿');
+insert into fandom_table (fandom) values ('원스');
+insert into fandom_table (fandom) values ('엑소엘');
+insert into fandom_table (fandom) values ('미라클');
+insert into fandom_table (fandom) values ('네버랜드');
+insert into fandom_table (fandom) values ('믿지');
+insert into fandom_table (fandom) values ('플로버');
+insert into fandom_table (fandom) values ('모아');
+insert into fandom_table (fandom) values ('블링크');
+insert into fandom_table (fandom) values ('몬베베');
+insert into fandom_table (fandom) values ('마이');
+insert into fandom_table (fandom) values ('엔시티즌');
+
